7e53d56d746527c9b1a6b1d401f2d409ed758e01
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-08-23 23:33:11 +0500

    make the ui of chat

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { PiChatCircleLight } from "react-icons/pi";
+import "../style/faq.css";
 const Faq = () => {
   return (
     <div className='faq-wrapper'>
-        <div className='chat-wrapper'>
-            <PiChatCircleLight />
+        <div className="chat-wrapper">
+            <div className="chat-header">
+                <div className="heading">
+                    Chat Support
+                </div>
+                <div className="our-intro">
+                    <p>Izaan here. How can i help you</p>
+                </div>
+            </div>
+            <div className="chats">
+                <div className="display-message">
+
+                </div>
+            </div>
+        </div>
+        <div className='chat-icon'>
+            <PiChatCircleLight size={58} />
         </div>
     </div>
   )
