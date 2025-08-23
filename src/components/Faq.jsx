69f37214c65bc6ac00de554af34192171ee80c3a
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-08-23 23:05:19 +0500

    add chat icon

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { PiChatCircleLight } from "react-icons/pi";
 const Faq = () => {
   return (
-    <div>Faq</div>
+    <div className='faq-wrapper'>
+        <div className='chat-wrapper'>
+            <PiChatCircleLight />
+        </div>
+    </div>
   )
 }
 
