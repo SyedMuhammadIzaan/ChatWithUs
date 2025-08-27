0f58bc1c5fb8b2c4f408066fb25b8de144d4669d
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-08-27 23:23:56 +0500

    set the ui of chat

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { PiChatCircleLight } from "react-icons/pi";
 import "../style/faq.css";
 const Faq = () => {
+    const handleMessage=()=>{
+
+    }
   return (
     <div className='faq-wrapper'>
         <div className="chat-wrapper">
             <div className="chat-header">
                 <div className="heading">
-                    Chat Support
+                    <h3>Chat Support</h3>
                 </div>
                 <div className="our-intro">
                     <p>Izaan here. How can i help you</p>
@@ -15,11 +18,18 @@ const Faq = () => {
             </div>
             <div className="chats">
                 <div className="display-message">
-
+                    <div className="my-message">
+                        <p>Hi</p>
+                    </div>
+                    <div className="user-message">
+                        <p>Can you tell about refund policy</p>
+                    </div>
                 </div>
             </div>
             <div className="text-area-wrapper">
-                <div className="text-field"></div>
+                <div className="text-field">
+                    <input type="text" placeholder='Type Message...' name='message' onChange={handleMessage} />
+                </div>
                 <div className="msg-btn">
                     <button>Send</button>
                 </div>
